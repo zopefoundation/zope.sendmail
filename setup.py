0427ab138d272e3398c803f917209498d27dc434
Andreas Jung <yet@gmx.de>
2008-07-05 09:40:31 +0000

    updated release notes based svn information (3.5.0b1 was still marked as unreleased, 3.5.0b2 was not mention in the release notes :->)

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='zope.sendmail',
-      version = '3.5.0b2dev',
+      version = '3.5.0',
       url='http://cheeseshop.python.org/pypi/zope.sendmail',
       license='ZPL 2.1',
       description='Zope sendmail',
