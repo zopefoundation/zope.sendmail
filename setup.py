701a55b18ddf6ba567cff69335d8a6fa22343ff7
Tres Seaver <tseaver@palladion.com>
2012-05-23 12:38:12 +0000

    Not yet ready for release.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ tests_require=[
 
 
 setup(name='zope.sendmail',
-      version='3.7.5',
+      version='3.7.5dev',
       url='http://pypi.python.org/pypi/zope.sendmail',
       license='ZPL 2.1',
       description='Zope sendmail',
