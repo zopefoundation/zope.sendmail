4cec7634a1f67ec59049ea50b44575ddab88fb03
Jason Madden <jamadden@gmail.com>
2017-08-28 10:33:00 -0500

    100% coverage for test_mailer.py

--- src/zope/sendmail/tests/test_mailer.py
+++ src/zope/sendmail/tests/test_mailer.py
@@ -13,16 +13,6 @@
 ##############################################################################
 """Tests for mailers.
 """
-import socket
-import unittest
-from zope.interface.verify import verifyObject
-from zope.sendmail.interfaces import ISMTPMailer
-from zope.sendmail.mailer import SMTPMailer
-
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from io import StringIO
 
 try:
     from socket import sslerror as SSLError
@@ -30,66 +20,85 @@ except ImportError:
     # Py3: The error location changed.
     from ssl import SSLError
 
+import unittest
+from zope.interface.verify import verifyObject
+from zope.sendmail.interfaces import ISMTPMailer
+from zope.sendmail.mailer import SMTPMailer
+
+
 # This works for both, Python 2 and 3.
 port_types = (int, str)
 
+class SMTP(object):
+
+    fail_on_quit = False
+
+    def __init__(self, h, p):
+        self.hostname = h
+        self.port = p
+        self.quitted = False
+        self.closed = False
+        assert isinstance(p, port_types)
+
+    def sendmail(self, f, t, m):
+        self.fromaddr = f
+        self.toaddrs = t
+        self.msgtext = m
+
+    def login(self, username, password):
+        self.username = username
+        self.password = password
+
+    def quit(self):
+        if self.fail_on_quit:
+            raise SSLError("dang")
+        self.quitted = True
+        self.close()
+
+    def close(self):
+        self.closed = True
+
+    def has_extn(self, ext):
+        return True
+
+    def ehlo(self):
+        self.does_esmtp = True
+        return (200, 'Hello, I am your stupid MTA mock')
+
+
+class SMTPWithNoEHLO(SMTP):
+    does_esmtp = False
+
+    def helo(self):
+        return (200, 'Hello, I am your stupid MTA mock')
+
+    def ehlo(self):
+        return (502, 'I don\'t understand EHLO')
+
+
 class TestSMTPMailer(unittest.TestCase):
 
+    SMTPClass = SMTP
+
+    def _makeSMTP(self, h, p):
+        self.smtp = self.SMTPClass(h, p)
+        return self.smtp
+
+
     def setUp(self, port=None):
-        global SMTP
-        class SMTP(object):
-
-            fail_on_quit = False
-
-            def __init__(myself, h, p):
-                myself.hostname = h
-                myself.port = p
-                myself.quitted = False
-                myself.closed = False
-                if not isinstance(p, port_types):
-                    raise socket.error("Int or String expected")
-                self.smtp = myself
-
-            def sendmail(self, f, t, m):
-                self.fromaddr = f
-                self.toaddrs = t
-                self.msgtext = m
-
-            def login(self, username, password):
-                self.username = username
-                self.password = password
-
-            def quit(self):
-                if self.fail_on_quit:
-                    raise SSLError("dang")
-                self.quitted = True
-                self.close()
-
-            def close(self):
-                self.closed = True
-
-            def has_extn(self, ext):
-                return True
-
-            def ehlo(self):
-                self.does_esmtp = True
-                return (200, 'Hello, I am your stupid MTA mock')
-
-            def starttls(self):
-                pass
-
-
+        self.smtp = None
         if port is None:
             self.mailer = SMTPMailer()
         else:
             self.mailer = SMTPMailer(u'localhost', port)
-        self.mailer.smtp = SMTP
+
+        self.mailer.smtp = self._makeSMTP
 
     def test_interface(self):
         verifyObject(ISMTPMailer, self.mailer)
 
     def test_send(self):
-        for run in (1,2):
+        for run in (1, 2):
             if run == 2:
                 self.setUp(u'25')
             fromaddr = 'me@example.com'
@@ -146,64 +155,26 @@ class TestSMTPMailer(unittest.TestCase):
         self.assertEqual(self.smtp.password, b'\xc3\xa9vil')
 
     def test_send_failQuit(self):
-        self.mailer.smtp.fail_on_quit = True
-        try:
-            fromaddr = 'me@example.com'
-            toaddrs = ('you@example.com', 'him@example.com')
-            msgtext = 'Headers: headers\n\nbodybodybody\n-- \nsig\n'
-            self.mailer.send(fromaddr, toaddrs, msgtext)
-            self.assertEqual(self.smtp.fromaddr, fromaddr)
-            self.assertEqual(self.smtp.toaddrs, toaddrs)
-            self.assertEqual(self.smtp.msgtext, msgtext)
-            self.assertTrue(not self.smtp.quitted)
-            self.assertTrue(self.smtp.closed)
-        finally:
-            self.mailer.smtp.fail_on_quit = False
+        SMTP.fail_on_quit = True
+        self.addCleanup(lambda: setattr(SMTP, 'fail_on_quit', False))
+
+        fromaddr = 'me@example.com'
+        toaddrs = ('you@example.com', 'him@example.com')
+        msgtext = 'Headers: headers\n\nbodybodybody\n-- \nsig\n'
+        self.mailer.send(fromaddr, toaddrs, msgtext)
+        self.assertEqual(self.smtp.fromaddr, fromaddr)
+        self.assertEqual(self.smtp.toaddrs, toaddrs)
+        self.assertEqual(self.smtp.msgtext, msgtext)
+        self.assertTrue(not self.smtp.quitted)
+        self.assertTrue(self.smtp.closed)
 
 
 class TestSMTPMailerWithNoEHLO(TestSMTPMailer):
 
-    def setUp(self, port=None):
-
-        class SMTPWithNoEHLO(SMTP):
-            does_esmtp = False
-
-            def __init__(myself, h, p):
-                myself.hostname = h
-                myself.port = p
-                myself.quitted = False
-                myself.closed = False
-                if not isinstance(p, port_types):
-                    raise socket.error("Int or String expected")
-                self.smtp = myself
-
-            def helo(self):
-                return (200, 'Hello, I am your stupid MTA mock')
-
-            def ehlo(self):
-                return (502, 'I don\'t understand EHLO')
-
-
-        if port is None:
-            self.mailer = SMTPMailer()
-        else:
-            self.mailer = SMTPMailer(u'localhost', port)
-        self.mailer.smtp = SMTPWithNoEHLO
+    SMTPClass = SMTPWithNoEHLO
 
     def test_send_auth(self):
-        # This test requires ESMTP, which we're intentionally not enabling
-        # here, so pass.
-        pass
+        self.skipTest("This test requires ESMTP, which we're intentionally not enabling")
 
     test_send_auth_unicode = test_send_auth
     test_send_auth_nonascii = test_send_auth
-
-def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestSMTPMailer))
-    suite.addTest(unittest.makeSuite(TestSMTPMailerWithNoEHLO))
-    return suite
-
-
-if __name__ == '__main__':
-    unittest.main()
