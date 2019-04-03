2b266998980cf35f56e5bb35a3ac0cab09976e52
Michael Howitz <mh@gocept.com>
2019-04-03 14:08:07 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ LONG_DESCRIPTION = (
 
 setup(
     name='zope.sendmail',
-    version='5.0',
+    version='5.1.dev0',
     url='https://github.com/zopefoundation/zope.sendmail',
     license='ZPL 2.1',
     description='Zope sendmail',
